c19bdd7465068057195f7568c7a70b441a0a16f6
fantomvoid <fantom7030303@gmail.com>
2026-04-26 16:58:02 +0300

    working with classes, started cmake

---
+++ Term2/plus/laba4/build/CMakeFiles/dnd.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dnd.dir/link.d"
+  "CMakeFiles/dnd.dir/main.cpp.o"
+  "CMakeFiles/dnd.dir/main.cpp.o.d"
+  "dnd"
+  "dnd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dnd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Term2/plus/laba4/src/main.cpp
+++ Term2/plus/laba4/src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "raylib.h"
 #include "funcs.hpp"
 #include "classes.hpp"
 
-int main(){
-  
+int main() {
+    InitWindow(800, 600, "dnd");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawText("Good!", 250, 280, 150, BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
