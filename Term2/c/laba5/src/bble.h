27bb983b68514309bd6677b1f5310307bea33de9
Fantom <fantom7030303@gmail.com>
2026-05-06 17:25:42 +0000

    WIP

--- Term2/c/laba5/src/bble.h
+++ Term2/c/laba5/src/bble.h
@@ -6,10 +6,17 @@ void swap(int *a, int *b) {
 
 
 void bblesort(int arr[], int n){
+   int right = n-1;
    int last_swap = 1;
-   for (int i; i-1 < n; i++){
-      if (arr[i] < arr[i+1]){
-         swap(&arr[i], &arr[i+1]);
+
+   while (right > 0) {
+      last_swap = 0;
+      for (int i = 0; i < right; i++){
+         if (arr[i] > arr[i+1]){
+            swap(&arr[i], &arr[i+1]);
+            last_swap = i;
+         }
       }
+      right = last_swap;
    }
 }
--- Term2/c/laba5/src/main.c
+++ Term2/c/laba5/src/main.c
@@ -1,11 +1,23 @@
-#include "bble.h"
-#include "prn.h"
+#include "massives.h"
+#include <stdio.h>
 
 int main(){
    int unsorted[] = {1,5,4,77,23,5,5};
-   int size = sizeof(unsorted)/sizeof(int);
+   int size = sizeof(unsorted)/sizeof(unsorted[0]);
 
-   prnarr(unsorted, size)
+   prnarr(unsorted, size);
    
-   bblesort(unsorted, size);
+   int choise = 0;
+   scanf("%d", &choise);
+   switch (choise) {
+      case 1:
+         bblesort(unsorted, size);
+         break;
+      case 2:
+         bin_insertion_sort(unsorted, size);
+         break;
+   }
+
+   printf("\n");
+   prnarr(unsorted, size);
 }
